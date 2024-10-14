2739f95a1077583a3853b741336ddbd47fbc40ec
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-10-14 20:15:45 -0300

    creating get endpoint

--- routes.js
+++ routes.js
@@ -5,6 +5,21 @@ router.get('/', (req, res) => {
     res.send("Hello World");
 });
 
+import pool from './db.js';
+
+
+router.get('/getCars', (req, res) =>{
+    pool.query('SELECT * FROM cars', (err, res) => {
+        if(err)
+        {
+            console.error("Error: ", err);
+        }
+        else
+        {
+            console.log('Resultado:', res.rows);
+        }
+    });
+});
 
 
 export default router;
