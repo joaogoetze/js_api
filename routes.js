3770725a62058ab1bf9d1b276564c2889cdabd42
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-10-15 07:53:53 -0300

    get car by id route made

--- routes.js
+++ routes.js
@@ -23,6 +23,29 @@ router.get('/readCars', (req, res) => {
     });
 });
 
+router.get('/readCar/:car_id', (req, res) => {
+    const { car_id } = req.params;
+    console.log(car_id);
+    pool.query('SELECT * FROM cars WHERE car_id = $1', [car_id], (db_err, db_res) => {
+        if (db_err) 
+        {
+            console.error("Error: ", db_err);
+            res.status(500).json({ error: 'Database error' });
+        } 
+        else if (db_res.rows.length === 0) 
+        {
+            res.status(404).json({ message: 'Car not found' });
+            console.log(car_id);
+        } 
+        else 
+        {
+            console.log('Resultado:', db_res.rows[0]);
+            res.json(db_res.rows[0]);
+        }
+    });
+});
+
+
 router.post('/createCars', (req, res) => {
     const { car_name } = req.body;
     pool.query('INSERT INTO cars (car_name) VALUES ($1) RETURNING *', [car_name], (db_err, db_res) => {
