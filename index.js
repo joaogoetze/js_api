9d09c73018a52d7a5eec5dce64e54eb8a6a7fb7a
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-11-01 19:24:08 -0300

    adding cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 
 import dotenv from 'dotenv';
@@ -8,9 +9,11 @@ import carRoutes from './routes/routes.js';
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use('/api', carRoutes);
 
-const PORT = 3000;
+const PORT = 3200;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
